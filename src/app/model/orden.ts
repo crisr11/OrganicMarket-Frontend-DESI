c97ca95c37316942ac8353ee816344361b979ccf
Jhomax21 <jhomaxtorres@gmail.com>
2022-09-17 00:22:25 -0500

    cambios orden.ts

---
+++ src/app/model/orden.ts
@@ -0,0 +1,11 @@
+import { agricultor } from "./agricultor";
+
+export class Vehiculo{
+  id_orden:number=0;
+  fechacompra:string="";
+  fechaenvio:string="";
+  fechaentrega: string="";
+  cantidad: number=0;
+  agricultor_id_agricultor:agricultor=new agricultor();
+  mayorista_id_mayorista: number=0; 
+}
